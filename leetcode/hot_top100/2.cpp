d33449e05c38db67f0a3dfc17e979eb4327b830a
ZephrysZheng <inverse_sky@163.com>
2021-07-18 23:42:15 +0800

    2.两数之和--链表

--- leetcode/hot_top100/2.cpp
+++ leetcode/hot_top100/2.cpp
@@ -7,3 +7,127 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode():val(0), next(nullptr){}
+    ListNode(int x):val(x),next(nullptr){}
+    ListNode(int x,ListNode *next):val(x),next(next){}
+};
+class Solution
+{
+    public:
+    ListNode * addTwoNumbers(ListNode * l1,ListNode *l2)
+    {
+        ListNode ans;
+        ListNode *anshead=&ans;
+        int flag=0;
+        while (l1!=nullptr&&l2!=nullptr)
+        {
+            ListNode *temp=new ListNode();
+            temp->val=l1->val+l2->val+flag;
+            flag=0;
+            if (temp->val>=10)
+            {
+                temp->val=temp->val%10;
+                flag=1;
+            }
+            anshead->next=temp;
+            anshead=anshead->next;
+            l1=l1->next;
+            l2=l2->next;
+        }
+        if (l1!=nullptr)
+        {
+            while (l1!=nullptr)
+            {
+            ListNode *temp=new ListNode();
+            temp->val=l1->val+flag;
+            flag=0;
+            if (temp->val>=10)
+            {
+                temp->val=temp->val%10;
+                flag=1;
+            }
+            anshead->next=temp;
+             anshead=anshead->next;
+            l1=l1->next;
+            }
+            
+        }
+        if (l2!=nullptr)
+        {
+             while (l2!=nullptr)
+            {
+            ListNode *temp=new ListNode();
+            temp->val=l2->val+flag;
+            flag=0;
+            if (temp->val>=10)
+            {
+                temp->val=temp->val%10;
+                flag=1;
+            }
+            anshead->next=temp;
+             anshead=anshead->next;
+            l2=l2->next;
+            }
+            
+        }
+        if (flag==1)
+        {
+        ListNode *temp=new ListNode();
+            temp->val=flag;
+            anshead->next=temp;
+            flag=0;
+        }
+    return (&ans)->next;
+
+    }
+};
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    ListNode l1(-1);
+    ListNode l2(-1);
+    ListNode *l1head=&l1;
+    ListNode *l2head=&l2;
+    for (int i=0;i<n;i++)
+    {
+         if (l1head->val==-1)
+         {
+             cin>>l1head->val;
+         }
+         else
+         {
+             ListNode *temp=new ListNode();
+             cin>>temp->val;
+             l1head->next=temp;
+             l1head=l1head->next;
+         }
+    }
+        for (int i=0;i<m;i++)
+    {
+         if (l2head->val==-1)
+         {
+             cin>>l2head->val;
+         }
+         else
+         {
+             ListNode *temp=new ListNode();
+             cin>>temp->val;
+             l2head->next=temp;
+             l2head=l2head->next;
+         }
+    }
+    Solution s;
+    ListNode *ans=s.addTwoNumbers(&l1,&l2);
+    while (ans!=nullptr)
+    {
+        cout<<ans->val;
+        ans=ans->next;
+    }
+    return 0;
+}
